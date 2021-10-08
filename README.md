516453b61cf3efe0e6eff809313179aba3145aec
Lyubov Myetolkina <luba150278@gmail.com>
2021-10-08 08:28:52 +0300

    task4 finish

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # BackJS_SHPP_Course
-I am using the "Colors" library for a pretty color console. Install it if you run my code. 
+I am using the "Colors" library for a pretty color console. Install it if you run my code.  
 Library: https://www.npmjs.com/package/colors
 
-I am using the "Chance" library to generate random data for created classes. 
+I am using the "Chance" library to generate random data for created classes.  
 Library: https://chancejs.com/
+
+I'm using npm install readline-sync for get sync user data 
 # tasks:
-task1 - dataTypes.js - learn data types, constructor-function
-task2 - regular.js - learn regular expressions
-task3 - oop.js - learn classes, abstract classes, proptotype
-task4 - fp.js - functional programming
+1. task1 - dataTypes.js - learn data types, constructor-function  
+2. task2 - regular.js - learn regular expressions  
+3. task3 - oop.js - learn classes, abstract classes, proptotype  
+4. task4 - fp.js - functional programming  
---
+++ fp.js
@@ -0,0 +1,76 @@
+const readline = require("readline");
+const fs = require("fs");
+/**
+ * Get user console data
+ * @param {*} question - "Placeholder" - phrase for user interface
+ * @returns answer from user
+ */
+const question = (question) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      return resolve(answer);
+    });
+  });
+};
+/**
+ * Task:
+ * 1. User should input phrase which can contain city name.
+ * 2. Need to check the CSV file and find city data (population, place in TOP-10) if it is
+ * 3. Replace city name in the phrase on the data from CSV file and get a new phrase
+ * 4. If the phrase doesn't have equal in CSV file output it as it.
+ */
+async function main() {
+  /**
+   * closure functions which get CSV file and return function witch replace start text ("phrase") by new phrase.
+   * The new phrase should contain data from a CSV file if the start text has coincident parts.
+   * If it isn't function should return the start text
+   * @param {*} csv - file
+   * @returns result see decrpiption below
+   */
+  function csvMode(csv) {
+    const cities = csv
+      .split("\n")
+      .filter((s) => s.trim() != "" && !s.startsWith("#"))
+      .map((arr) => {
+        let s = arr.split(",");
+        return [s[2], +s[3]];
+      })
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 10);
+    /**
+     * function - replace city in start text by CSV data
+     */
+    return (startText) => {
+      const result = startText.split(" ").map((word) => {
+        const index = cities.findIndex((item) => item[0] == word);
+
+        if (index != -1) {
+          return `${cities[index][0]} (${
+            index + 1
+          } место в ТОП-10 самых крупных городов Украины, население ${
+            cities[index][1]
+          } человек)`;
+        } else {
+          return word;
+        }
+      });
+      return result.join(" ");
+    };
+  }
+  const answer = await question("Введите свою фразу: ");
+  const phrase = answer || "Київ";
+
+  try {
+    const file = fs.readFileSync("./tests/cities.csv", "utf8"); //read file
+    console.log(csvMode(file)(phrase)); //modify CSV data and get result
+  } catch (err) {
+    console.error(err);
+  }
+}
+main().catch(console.error);
